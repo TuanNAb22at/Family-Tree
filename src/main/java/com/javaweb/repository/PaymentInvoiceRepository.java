b170029f982b7265b892981ad5884f5422da8438
Nguyen Anh Tuan <youremail@example.com>
2026-02-22 20:43:49 +0700

    first commit

--- src/main/java/com/javaweb/repository/PaymentInvoiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javaweb.repository;
-
-import com.javaweb.entity.PaymentInvoiceEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentInvoiceRepository extends JpaRepository<PaymentInvoiceEntity,Long> {
-}
--- src/main/java/com/javaweb/repository/custom/RentalReceiptRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package com.javaweb.repository.custom;
-
-import com.javaweb.entity.RentalReceiptEntity;
-
-public interface RentalReceiptRepositoryCustom {
-    RentalReceiptEntity findRentalReceipt(String startDate, String endDate, Long customerId);
-}
